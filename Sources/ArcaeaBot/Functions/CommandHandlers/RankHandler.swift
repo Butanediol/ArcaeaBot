d0086a27fb92b52ccd1623bc10d18b36aceba8c0
Butanediol <n872485238@gmail.com>
2021-08-29 14:14:19 +0800

    Rank with name

--- Sources/ArcaeaBot/Functions/CommandHandlers/RankHandler.swift
+++ Sources/ArcaeaBot/Functions/CommandHandlers/RankHandler.swift
@@ -2,33 +2,32 @@ import Foundation
 import TelegramBotSDK
 
 func rankHandler(context: Context) -> Bool {
+    // Currently not available
 
-	// Currently not available
+    let adminList: [Int64] = [803_217_365, 254_213_545]
 
-	let adminList: [Int64] = [803217365, 254213545]
+    guard let fromId = context.fromId, adminList.contains(fromId) else { return true }
 
-	guard let fromId = context.fromId, adminList.contains(fromId) else { return true }
+    var respondText = ""
 
-	var respondText = ""
+    let allUserInfo = getAllUserInfo()
 
-	let allUserInfo = getAllUserInfo()
+    for index in allUserInfo.indices {
+        let (tgUserId, userInfo) = allUserInfo[index]
+        if userInfo.status == -1 {
+            deleteUserInfoFromDatabase(tgUserId: tgUserId)
+        }
+        let rank = allUserInfo.rankingWithTie(index: index) {
+            $0.1.content.rating == $1.1.content.rating
+        }
+        respondText += String(format: "%@ %@ %.2f\n", intToStringRank(i: rank + 1), userInfo.content.name, Double(userInfo.content.rating) / 100)
+    }
 
-	for index in allUserInfo.indices {
-		let (tgUserId, userInfo) = allUserInfo[index]
-		if userInfo.status == -1 {
-			deleteUserInfoFromDatabase(tgUserId: tgUserId)
-		}
-		let rank = allUserInfo.rankingWithTie(index: index) {
-			$0.1.content.rating == $1.1.content.rating
-		}
-		respondText += String(format: "%@ %@ %.2f\n", intToStringRank(i: rank + 1), userInfo.content.name, Double(userInfo.content.rating) / 100)
-	}
+    context.respondAsync(respondText, replyToMessageId: context.message?.messageId) { message, _ in
+        if let chatId = context.chatId, let message = message {
+            deleteMessageAfter(deadline: .now() + 30, bot: bot, chatId: .chat(chatId), messageId: message.messageId)
+        }
+    }
 
-	context.respondAsync(respondText, replyToMessageId: context.message?.messageId) { message, _ in
-		if let chatId = context.chatId, let message = message {
-			deleteMessageAfter(deadline: .now() + 30, bot: bot, chatId: .chat(chatId), messageId: message.messageId)
-		}
-	}
-
-	return true
+    return true
 }
