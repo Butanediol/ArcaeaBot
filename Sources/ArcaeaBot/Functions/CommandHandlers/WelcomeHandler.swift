cb08537abc095b62dee8a21b69e8ccdb262f3c04
Butanediol <n872485238@gmail.com>
2021-09-30 12:41:42 +0800

    Disable admin check

--- Sources/ArcaeaBot/Functions/CommandHandlers/WelcomeHandler.swift
+++ Sources/ArcaeaBot/Functions/CommandHandlers/WelcomeHandler.swift
@@ -5,20 +5,22 @@ func welcomeHandler(context: Context) -> Bool {
     guard let message = context.message, let newUsers = message.newChatMembers else { return true }
     guard let chat = context.bot.getChatSync(chatId: .chat(context.chatId ?? 0)) else { return true }
 
-    if newUsers.contains(where: { user in
+    if newUsers.contains(where: { user in // check self
         user.id == context.bot.getMeSync()?.id
     }) {
-        guard let botAdmin = context.bot.getChatMemberSync(chatId: .chat(chat.id), userId: 254_213_545) else {
-            // chat contains bot admin?
-            print("cannot get chat member")
-            context.sendThenDeleteMessageAsync(text: "Unauthorized chat, leaving...")
-            context.bot.leaveChatAsync(chatId: .chat(chat.id))
-            return true
-        }
-        if [ChatMember.Status.kicked, ChatMember.Status.left, ChatMember.Status.restricted].contains(botAdmin.status) {
-            context.sendThenDeleteMessageAsync(text: "Unauthorized chat, leaving...")
-            context.bot.leaveChatAsync(chatId: .chat(chat.id))
-        }
+        /*
+         guard let botAdmin = context.bot.getChatMemberSync(chatId: .chat(chat.id), userId: 254_213_545) else {
+             // chat contains bot admin?
+             print("cannot get chat member")
+             context.sendThenDeleteMessageAsync(text: "Unauthorized chat, leaving...")
+             context.bot.leaveChatAsync(chatId: .chat(chat.id))
+             return true
+         }
+         if [ChatMember.Status.kicked, ChatMember.Status.left, ChatMember.Status.restricted].contains(botAdmin.status) {
+             context.sendThenDeleteMessageAsync(text: "Unauthorized chat, leaving...")
+             context.bot.leaveChatAsync(chatId: .chat(chat.id))
+         }
+         */
         return true
     }
 
