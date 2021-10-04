e08de5b17d4a11145a31fa818b79fde3b1cfd8d3
Butanediol <n872485238@gmail.com>
2021-10-04 22:27:17 +0800

    Change 1st emoji

--- Sources/ArcaeaBot/Functions/HelpFunctions/IntToStringRank.swift
+++ Sources/ArcaeaBot/Functions/HelpFunctions/IntToStringRank.swift
@@ -1,12 +1,12 @@
 func intToStringRank(i: Int) -> String {
-	switch i {
-		case 1:
-			return "🏅️"
-		case 2:
-			return "🥈"
-		case 3:
-			return "🥉"
-		default:
-			return String(format: "%02d", i)
-	}
+    switch i {
+    case 1:
+        return "🥇"
+    case 2:
+        return "🥈"
+    case 3:
+        return "🥉"
+    default:
+        return String(format: "%02d", i)
+    }
 }
