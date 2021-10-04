74fb2dbf2bf34588210930ac873d55295c673f7f
Butanediol <n872485238@gmail.com>
2021-10-04 22:28:00 +0800

    Unbind handler is able to unbind other user
    As long as no one read my code, this will be safe, right?

--- Sources/ArcaeaBot/Functions/CommandHandlers/UnbindHandler.swift
+++ Sources/ArcaeaBot/Functions/CommandHandlers/UnbindHandler.swift
@@ -2,20 +2,32 @@ import Foundation
 import TelegramBotSDK
 
 func unbindHandler(context: Context) -> Bool {
+    // /unbind [ArcName]
+    context.sendChatActionAsync(action: "typing")
 
-	context.sendChatActionAsync(action: "typing")
+    if let arg = context.args.scanWord() {
+        // With username provided
+        for (id, info) in getAllUserInfo() {
+            if info.content.name == arg {
+                deleteUserInfoFromDatabase(tgUserId: id)
+                context.respondAsync("再见，\(info.content.name)！", replyToMessageId: context.message?.messageId)
+                return true
+            }
+        }
+        context.sendThenDeleteMessageAsync(text: "未找到名为 \(arg) 的用户。")
+    } else {
+        guard let tgUserId = context.fromId else { return true }
 
-	guard let tgUserId = context.fromId else { return true }
+        guard let info = getUserInfoFromDatabase(tgUserId: tgUserId) else {
+            // Not binded
+            context.respondAsync("我不认识你，使用 /bind <ArcID> 绑定。")
+            return true
+        }
 
-	guard let info = getUserInfoFromDatabase(tgUserId: tgUserId) else {
-		// Not binded
-		context.respondAsync("我不认识你，使用 /bind <ArcID> 绑定。")
-		return true
-	}
+        deleteUserInfoFromDatabase(tgUserId: tgUserId)
 
-	deleteUserInfoFromDatabase(tgUserId: tgUserId)
+        context.respondAsync("再见，\(info.content.name)！", replyToMessageId: context.message?.messageId)
+    }
 
-	context.respondAsync("再见，\(info.content.name)！", replyToMessageId: context.message?.messageId)
-	
-	return true
+    return true
 }
