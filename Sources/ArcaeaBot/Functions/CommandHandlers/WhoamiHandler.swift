0b64de729d7c6e4629650d4f0f927eb072596fad
Butanediol <n872485238@gmail.com>
2021-11-14 22:37:23 +0800

    Rename command /me to /whoami

--- Sources/ArcaeaBot/Functions/CommandHandlers/WhoamiHandler.swift
+++ Sources/ArcaeaBot/Functions/CommandHandlers/WhoamiHandler.swift
@@ -1,7 +1,7 @@
 import Foundation
 import TelegramBotSDK
 
-func getMeHandler(context: Context) -> Bool {
+func whoamiHandler(context: Context) -> Bool {
     guard let tgUserId = context.fromId else { return true }
 
     if let info = getUserInfoFromDatabase(tgUserId: tgUserId) {
@@ -13,7 +13,7 @@ func getMeHandler(context: Context) -> Bool {
         )
     } else {
         context.respondAsync(
-            "我不认识你，使用 /bind <ArcID> 绑定。",
+            "我不认识你，使用 /bind <ArcID/ArcName> 绑定。",
             replyToMessageId: context.message?.messageId
         )
     }
